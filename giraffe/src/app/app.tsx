b92473318268f4a858884a3c16067fce29b9db75
ccq_com <chucongqing@gmail.com>
2020-03-20 17:17:28 +0800

    123

--- giraffe/src/app/app.tsx
+++ giraffe/src/app/app.tsx
@@ -4,12 +4,14 @@ import {Provider} from 'react-redux'
 import { store } from 'app/store'
 
 import UserInfoComp from 'app/components/userInfo/userInfo'
+import ZChart from './components/charts/zchart'
 
 const GiraffeApp = ()=>{
  
     return (
         <Provider store={store}>
             <UserInfoComp></UserInfoComp>
+            <ZChart></ZChart>
         </Provider>
         
     )
